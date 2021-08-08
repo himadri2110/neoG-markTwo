ed1a91fb458b96caa212abac185e8ac5d8eda570
Himadri Shah <himadrishah2000@gmail.com>
2021-08-08 15:30:17 +0530

    Feat: added empty input checks

--- index.js
+++ index.js
@@ -2,36 +2,39 @@ const readlineSync = require('readline-sync');
 const chalk = require('chalk');
 
 let score = 0;
-var game = chalk.bold.yellow('\'Do you really know India\'');
+let game = chalk.bold.yellow('\'Do you really know India?\'');
 
-var name = readlineSync.question(chalk.bold('Hey, What\'s your name? '));
-console.log(`Welcome, ${name}!`);
+let name = readlineSync.question(chalk.bold('Hey, What\'s your name? '));
 
-console.log('');
+while(name.length === 0) {
+	name = readlineSync.question(chalk.bold('Hey, What\'s your name? '));
+}
 
-console.log(`Let\'s play a game ${game}`);
+console.log(`Welcome, ${name}! \n`);
 
-console.log('');
+console.log(`Let\'s play a game: ${game} \n`);
 
-console.log(chalk.underline.bold.black.bgWhite('-- Level 1 --'));
+console.log(chalk.underline.bold.black.bgWhite('-- Level 1 --\n'));
 
-console.log('');
 function askQuestion(ques, ans, i) {
-	var userAns = readlineSync.question(chalk.bold(`${i}. ${ques}`));
+	let userAns = readlineSync.question(chalk.bold(`${i}. ${ques}`));
+
+	while(userAns.length === 0) {
+		userAns = readlineSync.question(chalk.bold(`${i}. ${ques}`));
+	}
 
 	if(userAns.toLowerCase() == ans.toLowerCase()) {
 		console.log(chalk.bold.greenBright('Correct Answer :)'));
 		score++;
 	} else {
 		console.log(chalk.bold.red('Incorrect Answer :('));
-		console.log(chalk.bold.greenBright(`The correct answer is ${ans}`));
+		console.log(chalk.bold.greenBright(`The correct answer is ${ans}.`));
 	}
 
-	console.log(chalk.underline(`Score: ${score}`));
-	console.log(" ");
+	console.log(chalk.underline(`Score: ${score} \n`));
 }
 
-var quesList = [
+let quesList = [
 	{
 		q: 'What is the capital of India? ',
 		a: 'New Delhi',
@@ -94,15 +97,13 @@ var quesList = [
 	},
 ];
 
-for(var i=0; i<quesList.length; i++) {
+for(let i=0; i<quesList.length; i++) {
 	askQuestion(quesList[i]["q"], quesList[i]["a"], i+1);
 
-	if(score == 5 && i!==quesList.length-1) {
-		console.log(chalk.underline.black.bold.bgWhite('-- Level 2 --'));
-		console.log(" ");
-	} else if (score == 10 && i!==quesList.length-1) {
-		console.log(chalk.underline.black.bold.bgWhite('-- Level 3 --'));
-		console.log(" ");
+	if(score === 5 && i !== quesList.length-1) {
+		console.log(chalk.underline.black.bold.bgWhite('-- Level 2 --\n'));
+	} else if (score === 10 && i !== quesList.length-1) {
+		console.log(chalk.underline.black.bold.bgWhite('-- Level 3 --\n'));
 	}
 }
 
@@ -111,5 +112,5 @@ if(score >= 13) {
 } else if(score <=7) {
 	console.log(chalk.underline.italic.bold.bgBlue(`Result: You scored ${score} points! You do not know India well :(`));
 } else {
-	console.log(chalk.underline.italic.bold.bgBlue(`Result: You scored ${score} points! You do not know India fairly well`));
+	console.log(chalk.underline.italic.bold.bgBlue(`Result: You scored ${score} points! You know India fairly well.`));
 }
